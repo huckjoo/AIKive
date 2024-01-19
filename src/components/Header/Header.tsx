6705da706df0031d48e551807f6b9a3f3b865b33
huckjoo <omp0109@naver.com>
2024-01-19 14:59:48 +0900

    feat: header 개발

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,12 @@
 import styles from './Header.module.css';
+import { RiRobot2Line } from 'react-icons/ri';
 
 const Header = () => {
-  return <div className={styles.test}>HEADER 영역</div>;
+  return (
+    <div className={styles.header}>
+      <RiRobot2Line className={styles.icon} />
+    </div>
+  );
 };
 
 export default Header;
