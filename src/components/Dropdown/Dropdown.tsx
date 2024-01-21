230edeff46858419b5c64d55a31f05108ffdf961
huckjoo <omp0109@naver.com>
2024-01-21 17:13:36 +0900

    feat: 밖에 클릭하면 dropdown 닫히는 기능 구현

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from '@/components/Dropdown/Dropdown.module.css';
 import { RiArrowDropDownLine } from 'react-icons/ri';
 import { RiArrowDropUpLine } from 'react-icons/ri';
@@ -7,15 +7,31 @@ import clsx from 'clsx';
 const Dropdown = ({ options, name }: { options: string[]; name: string }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState('');
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const toggleDropdown = () => setIsOpen(!isOpen);
+
   const handleOptionClick = (option: string) => {
     setSelectedOption(option);
     setIsOpen(false);
   };
 
+  const handleClickOutside = (e: MouseEvent) => {
+    const target = e.target as HTMLElement; // TODO 단언문 삭제
+    if (dropdownRef.current && !dropdownRef.current.contains(target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className={styles.dropdown}>
+    <div className={styles.dropdown} ref={dropdownRef}>
       <button
         className={clsx(
           styles.dropdownButton,
