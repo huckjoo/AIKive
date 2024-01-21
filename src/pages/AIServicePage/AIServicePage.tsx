705cd316bdd4b3a275ef4af56818040c4d7dbc6d
huckjoo <omp0109@naver.com>
2024-01-21 14:32:06 +0900

    feat: 필터 버튼 마크업 작성

--- src/pages/AIServicePage/AIServicePage.tsx
+++ src/pages/AIServicePage/AIServicePage.tsx
@@ -1,7 +1,37 @@
 import styles from '@/pages/AIServicePage/AIServicePage.module.css';
 
 const AIServicePage = () => {
-  return <div className={styles.container}>AIService Page</div>;
+  const serviceFilter = [
+    '전체',
+    '이미지AI',
+    '비디오AI',
+    '오디오AI',
+    '디자인AI',
+    '언어모델',
+    '기타',
+  ];
+  return (
+    <div className={styles.container}>
+      {/** TODO: 컴포넌트로 분리 */}
+      <h1 className={styles.title}>AI 서비스 아카이브</h1>
+      <h3 className={styles.subtitle}>
+        현존하는 모든 AI 서비스를 한 눈에 확인해 보세요
+      </h3>
+      <div className={styles.filterContainer}>
+        <div className={styles.serviceFilter}>
+          {serviceFilter.map((filterName) => {
+            return (
+              <button className={styles.filterButton}>{filterName}</button>
+            );
+          })}
+        </div>
+        <div>
+          <button>요금 필터</button>
+          <button>국가 필터</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AIServicePage;
